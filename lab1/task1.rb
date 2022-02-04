913c2ac64dec51003dec9cab60a6f521c9867d4d
Elizaveta Malezh <milkasps66@gmail.com>
2022-02-04 18:03:23 +0300

    task1.3

--- lab1/task1.rb
+++ lab1/task1.rb
@@ -9,3 +9,17 @@ print"Hello world!\n"
 puts"Hello, #{ARGV[0]}"
 
 puts String.instance_methods
+
+#task1.3
+print"Напишите свой любимый язык программирования: "
+lang=STDIN.gets.chomp
+case lang
+when "ruby"
+puts"А вы подлиза!"
+when "c++"
+puts"А вы хороши. Но скоро это будет ruby!"
+when "kotlin"
+puts"Круто, но скоро вашим любимым языком станет ruby!"
+else
+puts"Скоро им станет ruby!"
+end
