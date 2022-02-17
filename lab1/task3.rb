6291d71d58fdfd4bb2b986f68da38e8b05e9b2fb
Elizaveta Malezh <milkasps66@gmail.com>
2022-02-17 22:30:57 +0300

    task3.4.8

--- lab1/task3.rb
+++ lab1/task3.rb
@@ -54,68 +54,77 @@ end
 
 #task3.2
 # задание 3_2
-def get_array()
-  array = Array.new
-  print "Введите размер массива:"
-  n = eval(STDIN.gets.chomp)
-  puts "\n Введите элементы массива:"
-  for i in 0..n-1
-   array.append(eval(STDIN.gets.chomp))
-  end
-  return array
+# def get_array()
+#   array = Array.new
+#   print "Введите размер массива:"
+#   n = eval(STDIN.gets.chomp)
+#   puts "\n Введите элементы массива:"
+#   for i in 0..n-1
+#    array.append(eval(STDIN.gets.chomp))
+#   end
+#   return array
+# end
+#
+#
+# def list_from_file(name_file)
+#   File.open(name_file+".txt","r") do |file|
+#     list = Array.new
+#     n = eval(file.readline())
+#     for i in 0..n-1
+#       list.append(eval(file.readline()))
+#     end
+#     return list
+#   end
+# end
+#
+#
+# def args_choice()
+#   function = eval(ARGV[0])
+#   type = eval(ARGV[1])
+#   file = ARGV[2]
+#
+#   # 1 с клавы, 2 с файла
+#   if type == 1
+#     array = get_array()
+#   end
+#   if type == 2
+#     array = list_from_file(file)
+#   end
+#
+#   case function
+#   when 1
+#     result = min_in_list(array)
+#   when 2
+#     result = max_in_list(array)
+#   when 3
+#     result = sum_in_list(array)
+#   when 4
+#     result = prod_in_list(array)
+#   else
+#     result = 'Неверная функция'
+#   end
+#   return(print("Результат: " , result))
+# end
+#
+# puts "Выберите нужную функцию:
+# 1. Мин эл-т списка
+# 2. Макс эл-т списка
+# 3. Сумма эл-тов списка
+# 4. Произведение эл-тов списка"
+#
+# puts "Выберите нужный вариант:
+# 1. Ввод списка с клавиатуры
+# 2. Взять список из файла (как 3-й аргумент укажите адрес файла)
+# \n"
+#
+# #file_name = list
+# puts args_choice()
+
+#task3.4
+#task3.4.8
+def task8(arr)
+  first_min = [arr.index(arr.min)]
+  arr2 = arr
+  arr2[first_min[0]] = arr2.max+1
+  puts first_min << arr2.index(arr2.min)
 end
-
-
-def list_from_file(name_file)
-  File.open(name_file+".txt","r") do |file|
-    list = Array.new
-    n = eval(file.readline())
-    for i in 0..n-1
-      list.append(eval(file.readline()))
-    end
-    return list
-  end
-end
-
-
-def args_choice()
-  function = eval(ARGV[0])
-  type = eval(ARGV[1])
-  file = ARGV[2]
-
-  # 1 с клавы, 2 с файла
-  if type == 1
-    array = get_array()
-  end
-  if type == 2
-    array = list_from_file(file)
-  end
-
-  case function
-  when 1
-    result = min_in_list(array)
-  when 2
-    result = max_in_list(array)
-  when 3
-    result = sum_in_list(array)
-  when 4
-    result = prod_in_list(array)
-  else
-    result = 'Неверная функция'
-  end
-  return(print("Результат: " , result))
-end
-
-puts "Выберите нужную функцию:
-1. Мин эл-т списка
-2. Макс эл-т списка
-3. Сумма эл-тов списка
-4. Произведение эл-тов списка"
-
-puts "Выберите нужный вариант:
-1. Ввод списка с клавиатуры
-2. Взять список из файла (как 3-й аргумент укажите адрес файла)
-\n"
-
-#file_name = list
-puts args_choice()
